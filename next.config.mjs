766d99cfe0888ee6809e9bab8cf6fc04136f6efb
tejachennu <tejachennu223@gmail.com>
2025-11-27 12:58:43 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
 
-export default nextConfig
+  // 🔥 REQUIRED FOR AZURE APP SERVICE
+  output: "standalone",
+};
+
+export default nextConfig;
